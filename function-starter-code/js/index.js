b522ac86c35364acc833054a9affea7a2f3ce7c8
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-10-31 13:56:10 +0000

    completed lab

---
+++ function-starter-code/js/index.js
@@ -0,0 +1,36 @@
+
+
+function addTwo(n1 , n2) {
+  return n1 + n2;
+}
+
+function addThree(n1, n2, n3) {
+  return n1 + n2 + n3;
+}
+
+function multiplyTwo(n1,n2) {
+  return n1 * n2;
+}
+
+function increaseByPercentage(total, percentage) {
+  return total + (percentage/100 * total);
+}
+
+function reverseWord(word) {
+  return word.split("").reverse().join("");
+}
+
+function celciusToFahrenheit(celcius) {
+  return celcius * (9/5) + 32;
+}
+
+function fahrenheitToCelcius(fahr) {
+  return (fahr - 32) * (5/9);
+}
+
+// BONUS QUESTION
+function stripVowels(word) {
+  return word.replace(/[aeiou]/ig,"");
+}
+
+runTests();
